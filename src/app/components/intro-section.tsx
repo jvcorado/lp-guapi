b10b4c54125dd841c0a4f47546c0ce267633c634
jvcorado <jhonisilva545@gmail.com>
2024-05-28 08:43:18 -0300

    svg centralizado

--- src/app/components/intro-section.tsx
+++ src/app/components/intro-section.tsx
@@ -1,63 +1,58 @@
-"use client";
-import { track } from "@vercel/analytics";
-
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import ModeToggle from "./mode-toggle";
+import { cn } from "@/lib/utils";
 
-function IntroSection() {
+export default function IntroSection() {
   return (
-    <section id="intro" className="space-y-6 py-8 md:py-12 lg:py-32 relative">
-      <svg
-        viewBox="0 0 1024 1024"
-        className="absolute left-1/3 top-full md:left-1/2 md:top-1/4  -z-10 h-[16rem] w-[16rem] md:h-[54rem] md:w-[54rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
-        aria-hidden="true"
-      >
-        <circle
-          cx="512"
-          cy="512"
-          r="512"
-          fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
-          fillOpacity="0.7"
-        />
-        <defs>
-          <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-            <stop stopColor="#7775D6" />
-            <stop offset="1" stopColor="#E935C1" />
-          </radialGradient>
-        </defs>
-      </svg>
-      <div className="container flex max-w-[64rem] flex-col items-center text-center gap-4">
+    <section
+      id="intro"
+      className="relative flex flex-col items-center justify-center gap-10 bg-red-500"
+    >
+      <div className="absolute inset-0 top-40 flex items-center justify-center -z-10">
+        <svg
+          viewBox="0 0 1024 1024"
+          className="h-[20rem] w-[20rem] md:h-[54rem] md:w-[54rem] [mask-image:radial-gradient(closest-side,white,transparent)]"
+          aria-hidden="true"
+        >
+          <circle
+            cx="512"
+            cy="512"
+            r="512"
+            fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+            fillOpacity="0.7"
+          />
+          <defs>
+            <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+              <stop stopColor="#7775D6" />
+              <stop offset="1" stopColor="#E935C1" />
+            </radialGradient>
+          </defs>
+        </svg>
+      </div>
+      <div className="container flex max-w-[64rem] flex-col items-center text-center gap-7">
         <span className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium flex items-center">
-          📅 Incscrições encerradas.
+          📅 Inscrições encerradas.
           <ModeToggle />
         </span>
-        <h1 className="font-heading text-4xl md:text-6xl lg:text-7xl">
-          The Next Dev
-          <br />
-          Full-stack <span className="text-primary">Next.js 14</span>
+        <h1 className="font-heading text-4xl md:text-6xl lg:text-7xl text-center font-bold">
+          The Next Dev <br /> Full-stack{" "}
+          <span className="text-indigo-400">Next.js 14</span>
         </h1>
-        <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+        <p className="text-center max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
           Invista na sua carreira e aprenda a desenvolver aplicações que as
           grandes empresas estão utilizando. Faça parte da próxima turma da
           formação mais completa de Next.js 14.
         </p>
-        <div className="space-x-4">
+        <div className="grid grid-cols-1 md:grid-flow-col gap-3 items-center">
           <a
             href="#investment"
-            className="bg-indigo-400 py-2 px-4 text-md text-white rounded shadow-lg font-bold"
-            onClick={() => {
-              track("enrollment", { location: "intro" });
-            }}
+            className="bg-indigo-400 border-none light:text-black rounded-lg py-2 px-4 text-md text-white shadow-lg md:font-bold"
           >
-            Quero fazer parte
+            Quero fazer
           </a>
           <a
-            href="#feature"
+            href="#about"
             className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
-            onClick={() => {
-              track("features");
-            }}
           >
             Saiba mais
           </a>
@@ -66,5 +61,3 @@ function IntroSection() {
     </section>
   );
 }
-
-export default IntroSection;
